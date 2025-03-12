cb79f7fd0a261d0d282280d6b8dff1ac83164ef8
root <spicemasterD@gmail.com>
2025-03-12 20:38:27 +0000

    update baseURL for proper connection

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000", // Replace with your backend URL
+	baseURL: "http://143.198.235.252:8000", // Replace with your backend URL
   withCredentials: true, // Ensure cookies are included in requests
 });
 
